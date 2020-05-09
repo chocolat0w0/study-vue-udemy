d3db7c1e0be47de69995ab88a6c73f020c5d3389
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2020-05-09 16:23:58 +0900

    section 10

---
+++ Section 10/src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <form action="">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="new_quote">Quote</label>
+                <textarea name="new_quote" id="new_quote" rows="3" class="form-control" v-model="quote"></textarea>
+
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
