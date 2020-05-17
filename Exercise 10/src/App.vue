812f79d51a3e4eab3fbe64fbc529811dcd4a3f4e
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2020-05-17 16:30:28 +0900

    Section 12

---
+++ Exercise 10/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <span v-myon:click="clicked">Click me</span>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            'myon': {
+                bind(el, binding, vnode) {
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        },
+        methods: {
+            clicked() {
+                alert('clicked');
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
